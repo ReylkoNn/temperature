f9d2080a22acaac74782240ed593bfcfe3889acc
ReylkoNn <syarmaster@gmail.com>
2025-11-08 01:08:56 +0200

    Реализовал createTemperatureData в data.cpp

---
+++ data.cpp
@@ -0,0 +1,22 @@
+#include "data.h"
+#include <cstring>
+
+namespace temp {
+
+    TemperatureData* createTemperatureData(int count, const char* unit) {
+        if (count <= 0) return nullptr;  // Проверка на корректный размер
+        TemperatureData* data = new TemperatureData;
+        data->temperatures = new float[count];  // Выделение памяти для температур
+        data->count = count;
+        size_t len = std::strlen(unit);
+        data->unit = new char[len + 1];  // Выделение памяти для единицы
+        std::strcpy(data->unit, unit);
+        // Инициализация нулями
+        for (int i = 0; i < count; ++i) {
+            data->temperatures[i] = 0.0f;
+        }
+        return data;
+    }
+
+
+}  // namespace temp
